78a6af0811aa6abffe98cbba064cdf51f9120ccc
Remus Bulboaca <remus.bulboaca@gmail.com>
2019-04-14 22:02:55 +0300

    Game menu finalizat pe partea de interfata. Script pe submeniu limba si dificultate -> coming soon

---
+++ GameMenu/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+
+{
+    
+    public void MuteSound()
+    {
+        AudioListener.pause = !AudioListener.pause;
+    }
+
+}
