033bc4e26a3608709b183a7785008ae73d0d7619
Irina-Elena <48416541+Irina-Elena@users.noreply.github.com>
2019-04-20 22:36:01 +0300

    alte teste adaugate
    testam daca atunci cand apasam butonul corect, raspunsul de pe buton este chiar cel corect

--- Trivia02/Trivia/Assets/Tests/FirstTest.cs
+++ Trivia02/Trivia/Assets/Tests/FirstTest.cs
@@ -20,6 +20,103 @@ namespace Tests
 
         }
 
+        [UnityTest]
+        public IEnumerator TestScriptForRightAnswerButton1()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            SceneManager.LoadScene("MainLevel");
+            yield return new WaitForSeconds(1);
+
+            Button[] buttons = GameObject.FindObjectsOfType<Button>();
+            Button b11 = buttons[0];
+            string rightAnswer = b11.GetComponentInChildren<Text>().text.ToString();
+        
+            var question = GameObject.Find("Question");
+            List<Question> expectedQuestions = GameManager.questions;
+
+            string expectedAnswer;
+
+            for (int i = 0; i < expectedQuestions.Count; i++)
+            {
+                
+                if (question.GetComponentInChildren<Text>().text == expectedQuestions[i].question)
+                {
+                    expectedAnswer = expectedQuestions[i].getRightAnswer();
+                     b11.onClick.Invoke();
+                    if (b11.GetComponentInChildren<Text>().text == "Correct!")
+                        Assert.AreEqual(expectedAnswer, rightAnswer);
+                    else
+                        Assert.AreNotEqual(expectedAnswer, rightAnswer);
+                }
+            }
+        }
+
+        [UnityTest]
+        public IEnumerator TestScriptForRightAnswerButton2()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            SceneManager.LoadScene("MainLevel");
+            yield return new WaitForSeconds(1);
+
+            Button[] buttons = GameObject.FindObjectsOfType<Button>();
+            Button b = buttons[1];
+            string rightAnswer = b.GetComponentInChildren<Text>().text.ToString();
+
+            var question = GameObject.Find("Question");
+            List<Question> expectedQuestions = GameManager.questions;
+
+            string expectedAnswer;
+
+            for (int i = 0; i < expectedQuestions.Count; i++)
+            {
+
+                if (question.GetComponentInChildren<Text>().text == expectedQuestions[i].question)
+                {
+                    expectedAnswer = expectedQuestions[i].getRightAnswer();
+                    b.onClick.Invoke();
+                    if (b.GetComponentInChildren<Text>().text == "Correct!")
+                        Assert.AreEqual(expectedAnswer, rightAnswer);
+                    else
+                        Assert.AreNotEqual(expectedAnswer, rightAnswer);
+                }
+            }
+        }
+
+        [UnityTest]
+        public IEnumerator TestScriptForRightAnswerButton3()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            SceneManager.LoadScene("MainLevel");
+            yield return new WaitForSeconds(1);
+
+            Button[] buttons = GameObject.FindObjectsOfType<Button>();
+            Button b = buttons[2];
+            string rightAnswer = b.GetComponentInChildren<Text>().text.ToString();
+
+            var question = GameObject.Find("Question");
+            List<Question> expectedQuestions = GameManager.questions;
+
+            string expectedAnswer;
+
+            for (int i = 0; i < expectedQuestions.Count; i++)
+            {
+
+                if (question.GetComponentInChildren<Text>().text == expectedQuestions[i].question)
+                {
+                    expectedAnswer = expectedQuestions[i].getRightAnswer();
+                    b.onClick.Invoke();
+                    if (b.GetComponentInChildren<Text>().text == "Correct!")
+                        Assert.AreEqual(expectedAnswer, rightAnswer);
+                    else
+                        Assert.AreNotEqual(expectedAnswer, rightAnswer);
+                }
+            }
+        }
+
+
         // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
         // `yield return null;` to skip a frame.
         [UnityTest]
@@ -35,17 +132,17 @@ namespace Tests
             var b3 = GameObject.Find("Option3");
             Button b11 = GameObject.FindObjectOfType<Button>();
             b11.onClick.Invoke();
-           // var b22 = b2.GetComponent<Button>();
-           // var b33 = b2.GetComponent<Button>();
+            // var b22 = b2.GetComponent<Button>();
+            // var b33 = b2.GetComponent<Button>();
             var question = GameObject.Find("Question");
             Debug.Log(b11.GetComponentInChildren<Text>().text);
             //b11.onClick.Invoke();
             /*JsonToObject desirializer = new JsonToObject();
             QuestionArray expectedQuestions = desirializer.loadJson();*/
             List<Question> expectedQuestions = GameManager.questions;
-            for(int i = 0; i < expectedQuestions.Count;i++)
+            for (int i = 0; i < expectedQuestions.Count; i++)
             {
-                if(question.GetComponentInChildren<Text>().text == expectedQuestions[i].question)
+                if (question.GetComponentInChildren<Text>().text == expectedQuestions[i].question)
                 {
                     Assert.AreEqual(expectedQuestions[i].getAllAnswers()[0], b1.GetComponentInChildren<Text>().text);
                     Assert.AreEqual(expectedQuestions[i].getAllAnswers()[1], b2.GetComponentInChildren<Text>().text);
@@ -53,8 +150,13 @@ namespace Tests
                 }
 
             }
-          
+
             //yield return null;
         }
+
+
+
+        //yield return null;
     }
 }
+
