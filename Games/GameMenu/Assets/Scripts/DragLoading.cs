4f80934745f4b62a746c0d1d7dd2e896104e6199
MosT98 <andreihalauca@yahoo.com>
2019-05-13 21:16:08 +0300

    fixed the android bug

--- Games/GameMenu/Assets/Scripts/DragLoading.cs
+++ Games/GameMenu/Assets/Scripts/DragLoading.cs
@@ -24,11 +24,12 @@ public class DragLoading : MonoBehaviour
 {
     [SerializeField]
     TextMeshProUGUI text;
+    List<string> DiskPaths { get; set; }
 
     public IEnumerator DownloadData(string url, string pathOnDisk)
     {
-        //Debug.Log(url);
-        // Debug.Log(pathOnDisk);
+        Debug.Log(url);
+        Debug.Log(pathOnDisk);
         var request = new UnityWebRequest(url, "GET");
         request.downloadHandler = new DownloadHandlerFile(pathOnDisk);
         request.certificateHandler = new CustomCertificateHandler();
@@ -40,52 +41,46 @@ public class DragLoading : MonoBehaviour
         }
     }
 
+    IEnumerator DownloadAllPhotos()
+    {
+        var stringBase = "https://armuseum.ml/uploads/photo/dragndrop/MuzeulMihaiEminescu/";
+        var photos = new string[]
+        {
+            "eminescu",
+            "bacovia",
+            "barbu",
+            "arghezi",
+            "alecsandri"
+        };
+
+        DiskPaths = new List<string>();
+
+        foreach (var i in photos)
+        {
+            var baseName = $"{i}.jpg";
+            var diskPath = $"{Application.persistentDataPath}/{baseName}";
+            DiskPaths.Add(diskPath);
+            yield return DownloadData($"{stringBase}/{baseName}", diskPath);
+        }
+    }
+
     IEnumerator GetTexture()
     {
+        yield return DownloadAllPhotos();
+
         text.GetComponentInChildren<TextMeshProUGUI>().text = "Loading";
-        string uri = "https://armuseum.ml/uploads/photo/dragndrop/MuzeulMihaiEminescu";
-        WebRequest request = WebRequest.Create(uri);
-        WebResponse response = request.GetResponse();
-        Regex regex = new Regex("<a href=\".*\">(?<name>.*.jpg)</a>");
-        List<string> links = new List<string>();
-        using (var reader = new StreamReader(response.GetResponseStream()))
+
+        foreach (var path in DiskPaths)
         {
-            string result = reader.ReadToEnd();
+            Debug.Log(path);
 
-            MatchCollection matches = regex.Matches(result);
-            if (matches.Count == 0)
-            {
-                Debug.Log("parse failed.");
-
-            }
-
-            foreach (Match match in matches)
-            {
-                if (!match.Success) { continue; }
-                links.Add((match.Groups["name"]).ToString());
-            }
-        }
-       
-        for (int x = 0; x < links.Count; x++)
-        {
-            if (x % 4 == 0)
-                text.GetComponentInChildren<TextMeshProUGUI>().text = "Loading";
-            else text.GetComponentInChildren<TextMeshProUGUI>().text = text.GetComponentInChildren<TextMeshProUGUI>().text + ".";
+            //if (x % 4 == 0)
+            //    text.GetComponentInChildren<TextMeshProUGUI>().text = "Loading";
+            //else text.GetComponentInChildren<TextMeshProUGUI>().text = text.GetComponentInChildren<TextMeshProUGUI>().text + ".";
             Images image = new Images();
-            image.setLink(links[x]);
-            Debug.Log(links[x]);
+            image.setLink(path);
 
-            var pathOnDisk = Application.persistentDataPath + "/Images/";
-
-            if (!Directory.Exists(pathOnDisk))
-                Directory.CreateDirectory(pathOnDisk);
-            //Debug.Log("The directory was created successfully at:" + Directory.GetCreationTime(pathOnDisk));
-
-            var imagesPath = pathOnDisk + new FileInfo(uri+links[x]).Name;
-
-            yield return DownloadData(uri + links[x], imagesPath);
-
-            WWW www = new WWW(imagesPath);
+            WWW www = new WWW("file:///" + path);
             while (!www.isDone)
                 yield return null;
             Texture2D myTexture = www.texture;
